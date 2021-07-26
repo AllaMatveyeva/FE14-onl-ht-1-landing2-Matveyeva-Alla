aaa1d21c8859683cb76a2e26692a669f0c509c6d
Alla Matveyeva <alla.matveyeva.job@gmail.com>
2021-07-26 11:32:36 +0300

    add slider

--- prepros/js/slider.js
+++ prepros/js/slider.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
   $(".slider").slick({
     dots: true,
     adaptiveHeight: true,
+    arrows: false,
+    adaptiveWeight: true,
     autoplay: true,
     autoplaySpeed: 2000,
     waitForAnimate: false,
-    appendDots: $(".slider-item-right"),
+    appendDots: $(".dots"),
   });
 });
